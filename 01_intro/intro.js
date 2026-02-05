ed9589828ac39412d84fcc5588a327b8aaf08290
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-02-05 22:56:13 +0500

    all the concepts related to APP DEVELOPMENT

---
+++ 01_intro/intro.js
@@ -0,0 +1,8 @@
+// In this course I will learn all the concepts related to APP DEVELOPMENT :
+
+// JavaScript Basics and Functions, Js ES6 Arrow Functions and Classes, Js Arrays, React Js Basics, Creating the first 
+// Application of React native cli and expo cli environment, RN components view, text, button, textinput, alert, More on RN 
+// components TouchableOpacity, TouchableHightlight, TouchableNoFeedback, Hooks, StyleSheet, ScrollView, useEffect, 
+// FlatList, Image, Pressable, Introduction to React Navigation, Stack Navigator, Tab Navigator, Drawer navigator & Nested 
+// navigators, Themes, Dark/Light Theme and custom themes, AsyncStorage, Expo secure store, Working on Fetch Api, 
+// Context Api etc, Real time database (Firebase) and authentication
