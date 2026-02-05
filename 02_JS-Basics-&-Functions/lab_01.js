52efde48569a012f8be81e59451eefbc3761a659
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-02-05 23:02:37 +0500

    lab basic concepts

---
+++ 02_JS-Basics-&-Functions/lab_01.js
@@ -0,0 +1,44 @@
+// Lab 01
+// JavaScript Basics and Functions
+
+
+// Objective:
+// The objective of this lab will learn the students about the basics of JavaScript programming basics and the
+// functions with the help of examples and learning tasks.
+
+
+// Activity Outcomes:
+// The activities provide hands - on practice with the following topics
+//  Working with JS basics (datatypes).
+//  Implementation of JS loops
+//  Implementation of JS functions
+
+
+// Instructor Note:
+// As a pre-lab activity, read Chapter 03 from the text book “JavaScript For Impatient Programmers, Edition
+// 2022”.
+
+
+// 1) Useful Concepts:
+
+// In this lab, we will learn about basic JS variables and functions. We will discuss these types of JS
+// variables.
+//  Number
+// var a = value;
+//  String
+// var b = “value”
+//  Object (Arrays)
+// var c = {key: value} pair
+
+// To declare a variable in JavaScript; we will use keyword “var”. Also in this lab we will look at JS
+// functions.
+
+// To write a function, we have function calling and its definition. To call a function in JS, we will write it as
+// follows.
+//  NameOfFunction() //for zero argument function
+//  NameOfFunction(argument) //for single argument function
+//  NameOfFunction(argument1, argument2,…..) //multiple argument function
+
+// 4
+// To define a function we will use the following syntax.
+//  function NameOfFunction() //keyword ‘function’ and followed by function name which is called. 
