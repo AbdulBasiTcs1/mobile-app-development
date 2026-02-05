cd4783b793a7a41ae15fe24956e308ecdf23ea65
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-02-05 23:14:05 +0500

    1. Get used to the editor, run first JS program. 2. Write biography about yourself and print on console. 3. use 'var' to store your biography in variables, use appropriate primitive types. 4. Create JS Object for your biography key-value pairs. At least 4-5 keys, nested JS Object for Address, DegreePrograms etc e.g. { name: 'Some Name', age: 34, address: { }, degreeProgram: {} } Print biography on console (do not print entire object as it is)

---
+++ 02_JS-Basics-&-Functions/activity_01.js
@@ -0,0 +1,41 @@
+// Create JS Object for your biography key-value pairs. At least 4-5 keys, nested JS Object for 
+// Address, DegreePrograms etc 
+
+var bio = {
+    name: "Abdul Basit",
+    gender : "Male",
+    age : 21,
+    address : {
+        home : "GBHP Attock Pakistan",
+        city : "Attock",
+        country : "Pakistan"
+    }
+}
+console.log(bio.address.city)
+
+// get used to the editor, run first JS program. 
+console.log("Hey its my first code program I am running without saying HW in JS:)")
+
+// Write biography about yourself and print on console.  
+let biographyOfFamily = {
+    name: "FBNGS" ,
+    members : 5,
+    father : "Gul Shereen Khan",
+    mother : "Nasreen Akhtar" ,
+    sister : "Shumaila Shereen" ,
+    brother : "Abdul Fatir" ,
+    me : "Abdul Basit" ,
+    total : {
+        1 : "Gul Shereen Khan ",
+        2 : "Nasreen Akhtar",
+        3 : "Abdul Basit",
+        4 : "Abdul Fatir",
+        5 : "Shumaila Shereen"
+    },
+    contacts : {
+        number1 : "03458640708",
+        number2 : "03474288135"
+    },
+    location : "GBHP and ARF KAMRA"
+}
+console.log(biographyOfFamily)
