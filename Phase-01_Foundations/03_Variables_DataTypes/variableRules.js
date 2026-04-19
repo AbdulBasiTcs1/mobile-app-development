0a276bded280efa9e658933767efd8acc56e5731
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-04-19 18:47:27 +0500

    variables rules

---
+++ Phase-01_Foundations/03_Variables_DataTypes/variableRules.js
@@ -0,0 +1,6 @@
+// There are some of the rules for naming the JS Variables :
+// 1. no reserved words
+// 2. not start with the numbers
+// 3. no other than $,_,a-z,A-Z 
+// 4. no space between the Variables namings 
+// 5. use the camelCase for the varaibles naming bcz it is preffered to be used
